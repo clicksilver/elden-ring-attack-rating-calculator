239c65d01c459422a047c8f1ca1bb5db3b82fe2c
clicksilver <generho@gmail.com>
2022-03-27 10:42:43 -0700

    refactor scaling config lookup into weapon info lookup

--- lookup_weapon_name_affinity_info.py
+++ lookup_weapon_name_affinity_info.py
@@ -8,31 +8,54 @@ Formula ID: IDs for each of the main damage types
 Scaling ID: ID which maps to the scaling configuration of the weapon (which
             decides if a weapon's damage-type scales with each stat).
 """
-import lookup_scaling_id_to_config as scaling_config
+VERBOSE = False
 
-def init_table():
-    with open('data/CalcCorrectGraph_ID.csv') as f:
-        lines = f.readlines();
-        for i in range(len(lines)-1):
-            line = lines[i+1]
-            tokens = line.split(',')
-            wep_aff_name = tokens[0]
-            wep_info = [int(elem) for elem in tokens[1:]]
-            _TABLE[wep_aff_name] = wep_info
-    return
+def convertToBool(val):
+    return val == 1
 
-_TABLE = {}
-init_table()
+class StatScalingConfig:
+    def __init__(self, values):
+        self.scales_with_stre = convertToBool(values[0])
+        self.scales_with_dext = convertToBool(values[1])
+        self.scales_with_inte = convertToBool(values[2])
+        self.scales_with_fait = convertToBool(values[3])
+        self.scales_with_arca = convertToBool(values[4])
+
+    def __str__(self):
+        return '{:<5} {:<5} {:<5} {:<5} {:<5}'.format(
+                self.scales_with_stre,
+                self.scales_with_dext,
+                self.scales_with_inte,
+                self.scales_with_fait,
+                self.scales_with_arca)
+
+class ScalingConfig:
+    def __init__(self, values):
+        self.phys = StatScalingConfig(values[ 0: 5])
+        self.magi = StatScalingConfig(values[ 5:10])
+        self.fire = StatScalingConfig(values[10:15])
+        self.litg = StatScalingConfig(values[15:20])
+        self.holy = StatScalingConfig(values[20:25])
+
+    def __str__(self):
+        return ('{:4}: {:5} {:5} {:5} {:5} {:5}\n' +
+                'PHYS: {}\n' +
+                'MAGI: {}\n' +
+                'FIRE: {}\n' +
+                'LITG: {}\n' +
+                'HOLY: {}').format('TYPE', 'STR', 'DEX', 'INT', 'FAI', 'ARC',
+                        str(self.phys), str(self.magi), str(self.fire),
+                        str(self.litg), str(self.holy))
 
 class WeaponInfo:
-    def __init__(self, name, values):
+    def __init__(self, name, formula_id_list, scaling_config):
         self.name = name
-        self.phys_formula_id = values[0]
-        self.magi_formula_id = values[1]
-        self.fire_formula_id = values[2]
-        self.litg_formula_id = values[3]
-        self.holy_formula_id = values[4]
-        self.scaling_config = scaling_config.GetScalingConfig(values[-1])
+        self.phys_formula_id = formula_id_list[0]
+        self.magi_formula_id = formula_id_list[1]
+        self.fire_formula_id = formula_id_list[2]
+        self.litg_formula_id = formula_id_list[3]
+        self.holy_formula_id = formula_id_list[4]
+        self.scaling_config = scaling_config
 
     def __str__(self):
         return '{}\n{},{},{},{},{}\n{}'.format(self.name,
@@ -43,13 +66,37 @@ class WeaponInfo:
                 self.holy_formula_id,
                 self.scaling_config)
 
+def init_table():
+    stat_scaling_table = {}
+    with open('data/AttackElementCorrectParam.csv') as f:
+        lines = f.readlines();
+        for i in range(len(lines)-1):
+            line = lines[i+1]
+            tokens = line.split(',')
+            scaling_id = int(tokens[0])
+            stat_scaling_config = ScalingConfig([int(elem) for elem in tokens[1:]])
+            stat_scaling_table[scaling_id] = stat_scaling_config
+    with open('data/CalcCorrectGraph_ID.csv') as f:
+        lines = f.readlines();
+        for i in range(len(lines)-1):
+            line = lines[i+1]
+            tokens = line.split(',')
+            wep_aff_name = tokens[0]
+            values = [int(elem) for elem in tokens[1:]]
+            scaling_id = values[-1]
+            stat_scaling_config = stat_scaling_table[scaling_id]
+            _TABLE[wep_aff_name] = WeaponInfo(wep_aff_name, values, stat_scaling_config)
+    return
+
+_TABLE = {}
+init_table()
+
 def GetWeaponInfo(name, affinity=None):
     if affinity:
         key = '{} {}'.format(affinity, name)
     else:
         key = name
-    entry = _TABLE[key]
-    return WeaponInfo(key, entry)
+    return _TABLE[key]
 
 if __name__ == "__main__":
-    print(GetWeaponInfo('Arbalest'))
+    print(GetWeaponInfo('Bloody Helice'))
--- weapon_damage.py
+++ weapon_damage.py
@@ -9,7 +9,6 @@ Computes the weapon damage breakdown given:
 import stats
 
 import lookup_weapon_name_affinity_upgrade_base_rating as war
-import lookup_scaling_id_to_config as sc
 import lookup_weapon_name_affinity_upgrade_stat_scaling_factor as ssf
 import lookup_weapon_name_affinity_info as wif
 import weapon_formulas as wf
@@ -63,11 +62,11 @@ def ComputeWeaponParams(name, affinity, level, stats):
 
     # phys
     phys_fid = wep_info.phys_formula_id
-    phys_stre_fac = wisc.phys.stre * wf.GetCorrectionFactor(stats.stre, phys_fid)
-    phys_dext_fac = wisc.phys.dext * wf.GetCorrectionFactor(stats.dext, phys_fid)
-    phys_inte_fac = wisc.phys.inte * wf.GetCorrectionFactor(stats.inte, phys_fid)
-    phys_fait_fac = wisc.phys.fait * wf.GetCorrectionFactor(stats.fait, phys_fid)
-    phys_arca_fac = wisc.phys.arca * wf.GetCorrectionFactor(stats.arca, phys_fid)
+    phys_stre_fac = wisc.phys.scales_with_stre * wf.GetCorrectionFactor(stats.stre, phys_fid)
+    phys_dext_fac = wisc.phys.scales_with_dext * wf.GetCorrectionFactor(stats.dext, phys_fid)
+    phys_inte_fac = wisc.phys.scales_with_inte * wf.GetCorrectionFactor(stats.inte, phys_fid)
+    phys_fait_fac = wisc.phys.scales_with_fait * wf.GetCorrectionFactor(stats.fait, phys_fid)
+    phys_arca_fac = wisc.phys.scales_with_arca * wf.GetCorrectionFactor(stats.arca, phys_fid)
     phys_stre_dmg = phys_ar * stre_fac * phys_stre_fac / 100
     phys_dext_dmg = phys_ar * dext_fac * phys_dext_fac / 100
     phys_inte_dmg = phys_ar * inte_fac * phys_inte_fac / 100
@@ -77,11 +76,11 @@ def ComputeWeaponParams(name, affinity, level, stats):
 
     # magi
     magi_fid = wep_info.magi_formula_id
-    magi_stre_fac = wisc.magi.stre * wf.GetCorrectionFactor(stats.stre, magi_fid)
-    magi_dext_fac = wisc.magi.dext * wf.GetCorrectionFactor(stats.dext, magi_fid)
-    magi_inte_fac = wisc.magi.inte * wf.GetCorrectionFactor(stats.inte, magi_fid)
-    magi_fait_fac = wisc.magi.fait * wf.GetCorrectionFactor(stats.fait, magi_fid)
-    magi_arca_fac = wisc.magi.arca * wf.GetCorrectionFactor(stats.arca, magi_fid)
+    magi_stre_fac = wisc.magi.scales_with_stre * wf.GetCorrectionFactor(stats.stre, magi_fid)
+    magi_dext_fac = wisc.magi.scales_with_dext * wf.GetCorrectionFactor(stats.dext, magi_fid)
+    magi_inte_fac = wisc.magi.scales_with_inte * wf.GetCorrectionFactor(stats.inte, magi_fid)
+    magi_fait_fac = wisc.magi.scales_with_fait * wf.GetCorrectionFactor(stats.fait, magi_fid)
+    magi_arca_fac = wisc.magi.scales_with_arca * wf.GetCorrectionFactor(stats.arca, magi_fid)
     magi_stre_dmg = magi_ar * stre_fac * magi_stre_fac / 100
     magi_dext_dmg = magi_ar * dext_fac * magi_dext_fac / 100
     magi_inte_dmg = magi_ar * inte_fac * magi_inte_fac / 100
@@ -91,11 +90,11 @@ def ComputeWeaponParams(name, affinity, level, stats):
 
     # fire
     fire_fid = wep_info.fire_formula_id
-    fire_stre_fac = wisc.fire.stre * wf.GetCorrectionFactor(stats.stre, fire_fid)
-    fire_dext_fac = wisc.fire.dext * wf.GetCorrectionFactor(stats.dext, fire_fid)
-    fire_inte_fac = wisc.fire.inte * wf.GetCorrectionFactor(stats.inte, fire_fid)
-    fire_fait_fac = wisc.fire.fait * wf.GetCorrectionFactor(stats.fait, fire_fid)
-    fire_arca_fac = wisc.fire.arca * wf.GetCorrectionFactor(stats.arca, fire_fid)
+    fire_stre_fac = wisc.fire.scales_with_stre * wf.GetCorrectionFactor(stats.stre, fire_fid)
+    fire_dext_fac = wisc.fire.scales_with_dext * wf.GetCorrectionFactor(stats.dext, fire_fid)
+    fire_inte_fac = wisc.fire.scales_with_inte * wf.GetCorrectionFactor(stats.inte, fire_fid)
+    fire_fait_fac = wisc.fire.scales_with_fait * wf.GetCorrectionFactor(stats.fait, fire_fid)
+    fire_arca_fac = wisc.fire.scales_with_arca * wf.GetCorrectionFactor(stats.arca, fire_fid)
     fire_stre_dmg = fire_ar * stre_fac * fire_stre_fac / 100
     fire_dext_dmg = fire_ar * dext_fac * fire_dext_fac / 100
     fire_inte_dmg = fire_ar * inte_fac * fire_inte_fac / 100
@@ -105,11 +104,11 @@ def ComputeWeaponParams(name, affinity, level, stats):
 
     # litg
     litg_fid = wep_info.litg_formula_id
-    litg_stre_fac = wisc.litg.stre * wf.GetCorrectionFactor(stats.stre, litg_fid)
-    litg_dext_fac = wisc.litg.dext * wf.GetCorrectionFactor(stats.dext, litg_fid)
-    litg_inte_fac = wisc.litg.inte * wf.GetCorrectionFactor(stats.inte, litg_fid)
-    litg_fait_fac = wisc.litg.fait * wf.GetCorrectionFactor(stats.fait, litg_fid)
-    litg_arca_fac = wisc.litg.arca * wf.GetCorrectionFactor(stats.arca, litg_fid)
+    litg_stre_fac = wisc.litg.scales_with_stre * wf.GetCorrectionFactor(stats.stre, litg_fid)
+    litg_dext_fac = wisc.litg.scales_with_dext * wf.GetCorrectionFactor(stats.dext, litg_fid)
+    litg_inte_fac = wisc.litg.scales_with_inte * wf.GetCorrectionFactor(stats.inte, litg_fid)
+    litg_fait_fac = wisc.litg.scales_with_fait * wf.GetCorrectionFactor(stats.fait, litg_fid)
+    litg_arca_fac = wisc.litg.scales_with_arca * wf.GetCorrectionFactor(stats.arca, litg_fid)
     litg_stre_dmg = litg_ar * stre_fac * litg_stre_fac / 100
     litg_dext_dmg = litg_ar * dext_fac * litg_dext_fac / 100
     litg_inte_dmg = litg_ar * inte_fac * litg_inte_fac / 100
@@ -119,11 +118,11 @@ def ComputeWeaponParams(name, affinity, level, stats):
 
     # holy
     holy_fid = wep_info.holy_formula_id
-    holy_stre_fac = wisc.holy.stre * wf.GetCorrectionFactor(stats.stre, holy_fid)
-    holy_dext_fac = wisc.holy.dext * wf.GetCorrectionFactor(stats.dext, holy_fid)
-    holy_inte_fac = wisc.holy.inte * wf.GetCorrectionFactor(stats.inte, holy_fid)
-    holy_fait_fac = wisc.holy.fait * wf.GetCorrectionFactor(stats.fait, holy_fid)
-    holy_arca_fac = wisc.holy.arca * wf.GetCorrectionFactor(stats.arca, holy_fid)
+    holy_stre_fac = wisc.holy.scales_with_stre * wf.GetCorrectionFactor(stats.stre, holy_fid)
+    holy_dext_fac = wisc.holy.scales_with_dext * wf.GetCorrectionFactor(stats.dext, holy_fid)
+    holy_inte_fac = wisc.holy.scales_with_inte * wf.GetCorrectionFactor(stats.inte, holy_fid)
+    holy_fait_fac = wisc.holy.scales_with_fait * wf.GetCorrectionFactor(stats.fait, holy_fid)
+    holy_arca_fac = wisc.holy.scales_with_arca * wf.GetCorrectionFactor(stats.arca, holy_fid)
     holy_stre_dmg = holy_ar * stre_fac * holy_stre_fac / 100
     holy_dext_dmg = holy_ar * dext_fac * holy_dext_fac / 100
     holy_inte_dmg = holy_ar * inte_fac * holy_inte_fac / 100
