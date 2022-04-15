c75832da0b567fdfdb7e9ea7566efddab0f592b9
clicksilver <generho@gmail.com>
2022-04-14 22:33:41 -0700

    extend stats class to accept list of attributes

--- stats.py
+++ stats.py
@@ -3,9 +3,16 @@ Class container for stats array
 """
 
 class Stats:
-    def __init__(self, stre, dext, inte, fait, arca):
-        self.stre = stre
-        self.dext = dext
-        self.inte = inte
-        self.fait = fait
-        self.arca = arca
+    def __init__(self, stre, dext=None, inte=None, fait=None, arca=None):
+        if isinstance(stre, list):
+            self.stre = stre[0]
+            self.dext = stre[1]
+            self.inte = stre[2] 
+            self.fait = stre[3] 
+            self.arca = stre[4] 
+        else:
+            self.stre = stre
+            self.dext = dext
+            self.inte = inte
+            self.fait = fait
+            self.arca = arca
